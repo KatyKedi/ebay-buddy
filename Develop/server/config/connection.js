6191062c80e2459603254ff1bf66974975166f60
KatyKedi <katykat5bros@gmail.com>
2022-09-20 10:45:44 -0700

    edit config connection to troubleshoot heroku

--- Develop/server/config/connection.js
+++ Develop/server/config/connection.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+const connection = encodeURIComponent( process.env.MONGODB_URI )
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/ebay-buddy',
+  connection || 'mongodb://127.0.0.1:27017/ebay-buddy',
   {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
