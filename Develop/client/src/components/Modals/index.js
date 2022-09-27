7fcb0d39e0ca25bbae4866222f3e647efefac527
KatyKedi <katykat5bros@gmail.com>
2022-09-27 13:41:40 -0700

    modal styling

--- Develop/client/src/components/Modals/index.js
+++ Develop/client/src/components/Modals/index.js
@@ -4,119 +4,14 @@ import { UPDATE_CURRENT_MODAL } from '../../utils/actions';
 import { useQuery } from '@apollo/client'
 import { QUERY_ITEM } from '../../utils/queries';
 
-import { Modal, Button } from 'react-bootstrap'
+import { Modal, Button, Form } from 'react-bootstrap'
 import './style.css'
 
 
 function ItemModal() {
     const [state, dispatch] = useGlobalContext();
-    const { modal } = state
     const [modalOpen, setModal] = useState('item')
 
-    useEffect(() => {
-       
-            dispatch({
-                type: UPDATE_CURRENT_MODAL,
-                modal: modalOpen
-            })
-    }, [modalOpen])
-    
-    return  <Modal
-        show={modalOpen}>
-
-            <div className='form-row '>
-            <h2 className='col-12'>Enter Item Details</h2>
-            <form className='form-inline'>
-                <div className='col-12 form-group'>
-                    <label for="name">Name:</label>
-                    <input 
-                        type="text" 
-                        id="name"
-                        name="name" 
-                        required
-                        minlength="1"
-                        className='form-control'
-                    />
-                </div>
-                
-                <div className='col-12 form-group'>
-                    <label for="description">Description:</label>
-                    <input 
-                        type="text" 
-                        id="description"
-                        name="description"
-                        className='form-control'
-                    />
-                </div>
-                <div className='col-12 input-group'>
-                    <div className='input-group'>
-                    
-                    <div className='col-3'>
-                    <label for="length">Length:</label>
-                    <input 
-                        type="text"
-                        id="length"
-                        name="length"
-                        className='form-control'
-                    />
-                    </div>
-                    <div className='col-3'>
-                    <label for="width">Width:</label>
-                        <input
-                            type="text"
-                            id="width"
-                            name="width"
-                            className='form-control'
-                        />
-                    </div>
-                    <div className='col-3'>
-                    <label for="height">Height:</label>
-                    <input 
-                        type="text"
-                        id="height"
-                        name="height"
-                        className='form-control'
-                    />
-                    </div>
-                    <div className='col-3 form-group'>
-                        <label>Weight:</label>
-                        <input
-                            type="text"
-                            id="weight"
-                            name="weight"
-                            className='form-control'
-                        />
-                    </div>
-                    </div>
-                
-                    
-                </div>
-                <div className="input-group">
-                <div className='col-6'>
-                    <label for="section">Choose a section:</label>
-                    <select name="section" id="section" multiple className='form-control'>
-                        <option value=""></option>
-                    </select>
-                </div>
-                <div className='col-6'>
-                    <button type='button' className='btn btn-primary'>Submit</button>
-                    <Button 
-                    variant='danger' 
-                    onClick={() => {
-                        setModal('')
-                    }}>Close</Button>
-                </div>
-                </div>
-                
-            </form>
-        </div>
-    </Modal>
-}
-
-function SectionModal() {
-    const [state, dispatch] = useGlobalContext();
-    const [modalOpen, setModal] = useState('section')
-    
     useEffect(() => {
         dispatch({
             type: UPDATE_CURRENT_MODAL,
@@ -124,32 +19,145 @@ function SectionModal() {
         })
     }, [modalOpen])
 
-    return<Modal
+    return <Modal
+        show={modalOpen}
+        contentLabel="Add Item">
+
+        <Modal.Header closeButton>
+            <Modal.Title>Enter Item Details</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+
+            <Form>
+
+                <Form.Group className="mb-3" controlId="name">
+                    <Form.Label>Name:</Form.Label>
+                    <Form.Control
+                        type="name" placeholder="Enter name of item"
+                    />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="description">
+                    <Form.Label>Description:</Form.Label>
+                    <Form.Control
+                        type="description" placeholder="Enter item description"
+                    />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="size">
+
+                    <Form.Label>Length:</Form.Label>
+                    <Form.Control
+                        type="length"
+                    />
+
+                    <Form.Label>Width:</Form.Label>
+                    <Form.Control
+                        type="width"
+                    />
+
+                    <Form.Label>Height:</Form.Label>
+                    <Form.Control
+                        type="height"
+                    />
+
+                    <Form.Group>
+                        <Form.Label>Weight:</Form.Label>
+                        <Form.Control
+                            type="weight"
+                        />
+                    </Form.Group>
+
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId='section'>
+                    <Form.Label>Choose a section:</Form.Label>
+                    <Form.Select name="section" id="section" multiple className='form-control'>
+                        <option value="">Option</option>
+                        <option value="">Option</option>
+                        <option value="">Option</option>
+                    </Form.Select>
+                </Form.Group>
+
+            </Form>
+
+        </Modal.Body>
+
+        <Modal.Footer>
+            <Button
+                variant='success'
+                onClick={() => {
+                    setModal('')
+                }}
+            >Submit</Button>
+            <Button
+                variant='danger'
+                onClick={() => {
+                    setModal('')
+                }}
+            >Close</Button>
+        </Modal.Footer>
+    </Modal>
+}
+
+function SectionModal() {
+    const [state, dispatch] = useGlobalContext();
+    const [modalOpen, setModal] = useState('section')
+
+    useEffect(() => {
+        dispatch({
+            type: UPDATE_CURRENT_MODAL,
+            modal: modalOpen
+        })
+    }, [modalOpen])
+
+    return <Modal
         show={modalOpen}
         contentLabel="Add Section">
-            <h2>Enter Section Details</h2>
-            <form>
-            <label for="name">Name:</label>
-            <input 
-                type="text" 
-                id="name"
-                name="name" 
-                required
-                minlength="1"/>
-            <label for="full">Completely full:</label>
-            <input 
-                type="checkbox" 
-                id="full"
-                name="full" 
-                />
+        <Modal.Header closeButton>
+            <Modal.Title>Enter Section Details</Modal.Title>
+        </Modal.Header>
 
-            <button>Submit</button>
-        </form>
-        <button onClick={() => {
-                setModal('')
-        }}
-            >Close</button>
-        </Modal>
+        <Modal.Body>
+
+            <Form>
+
+                <Form.Group className='mb-3' controlId='name'>
+                    <Form.Label>Name:</Form.Label>
+                    <Form.Control
+                        type="name"
+                    />
+                </Form.Group>
+
+                <Form.Group className='mb-3' controlId='full'>
+                    <Form.Check
+                        type="checkbox"
+                        id="full"
+                        label="Completely Full?"
+                    />
+                </Form.Group>
+
+            </Form>
+            
+        </Modal.Body>
+
+        <Modal.Footer>
+            <Button
+                variant='success'
+                onClick={() => {
+                    setModal('')
+                }}
+            >Submit</Button>
+            <Button
+                variant='danger'
+                onClick={() => {
+                    setModal('')
+                }}
+            >Close</Button>
+        </Modal.Footer>
+
+    </Modal>
 }
 
 export { ItemModal, SectionModal } 
