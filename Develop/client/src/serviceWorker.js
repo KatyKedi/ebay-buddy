aac9de8b3bfbd786933ae72c5aa3098a10c82391
KatyKedi <katykat5bros@gmail.com>
2022-11-08 13:13:14 -0800

    gary nonsense

--- Develop/client/src/serviceWorker.js
+++ Develop/client/src/serviceWorker.js
@@ -16,6 +16,7 @@ export function register(config) {
 
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
+      console.log("no go")
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
       // serve assets; see https://github.com/facebook/create-react-app/issues/2374
@@ -24,8 +25,10 @@ export function register(config) {
 
     window.addEventListener('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/serviceWorker.js`;
+      console.log("Localhost: " + isLocalhost)
 
       if (isLocalhost) {
+        console.log("yep, this is localhost")
         // This is running on localhost. Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config);
 
@@ -46,12 +49,15 @@ export function register(config) {
 }
 
 function registerValidSW(swUrl, config) {
+  console.log(swUrl)
+  console.log(config)
   navigator.serviceWorker
     .register(swUrl, {scope: './'})
     .then(registration => {
       registration.onupdatefound = () => {
         const installingWorker = registration.installing;
         if (installingWorker == null) {
+          console.log("installing worker failed")
           return;
         }
         installingWorker.onstatechange = () => {
