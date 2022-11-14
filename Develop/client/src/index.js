a6ac1d0027ab41d88b96c30d3ff8092a5f02e8eb
KatyKedi <katykat5bros@gmail.com>
2022-11-14 09:59:48 -0800

    rebuild

--- Develop/client/src/index.js
+++ Develop/client/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './scss/custom.scss';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-createRoot(document.getElementById('root')).render(
-  <App  />
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
-serviceWorker.register();
+serviceWorkerRegistration.register();
